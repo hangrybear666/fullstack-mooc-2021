8cde94d07a02b1cee281d0992a08f1303a02990a
hangrybear666 <corey.lauster@gmail.com>
2022-02-23 20:30:12 +0100

    exercise 4.6 and 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -32,24 +32,77 @@ const favoriteBlog = (blogs) => {
 }
 
 const mostBlogs = (blogs) => {
-  // if (blogs && blogs.length === 0) {
-  //   return []
-  // } else if(blogs && blogs.length === 1) {
-  //   return blogs[0]
-  // } else {
-  //   const initialvalue = 0
-  //   const result = blogs.reduce((previousValue, currentArrayObject, currentIndex, array) => {
-  //     const maxIndex = array[currentIndex].likes > array[previousValue].likes ? currentIndex : previousValue
-  //     console.log("max Index ", maxIndex)
-  //     return maxIndex
-  //   }, initialvalue)
-  //   return blogs[result]
-  // }
+  let authorBlogCount = []
+  if (blogs && blogs.length === 0) {
+    return []
+  } else if(blogs && blogs.length === 1) {
+    authorBlogCount.push({
+      author: blogs[0].author,
+      blogs: 1
+    })
+    return authorBlogCount[0]
+  }
+  else {
+    let existingAuthorIndex = -1
+    blogs.forEach((blog, index, array) => {
+      existingAuthorIndex = authorBlogCount.map(e => e.author).indexOf(blog.author)
+      if (existingAuthorIndex !== -1) {
+        // console.log('author found!')
+        authorBlogCount[existingAuthorIndex].blogs += 1
+      } else {
+        authorBlogCount.push({
+          author: blog.author,
+          blogs: 1
+        })
+      }
+    })
+    const initialvalue = 0
+    const result = authorBlogCount.reduce((previousValue, currentArrayObject, currentIndex, array) => {
+      const maxIndex = array[currentIndex].blogs > array[previousValue].blogs ? currentIndex : previousValue
+      return maxIndex
+    }, initialvalue)
+    return authorBlogCount[result]
+  }
+}
+
+const mostLikes = (blogs) => {
+  let authorLikeCount = []
+  if (blogs && blogs.length === 0) {
+    return []
+  } else if(blogs && blogs.length === 1) {
+    authorLikeCount.push({
+      author: blogs[0].author,
+      likes: blogs[0].likes
+    })
+    return authorLikeCount[0]
+  }
+  else {
+    let existingAuthorIndex = -1
+    blogs.forEach((blog, index, array) => {
+      existingAuthorIndex = authorLikeCount.map(e => e.author).indexOf(blog.author)
+      if (existingAuthorIndex !== -1) {
+        // console.log('author found!')
+        authorLikeCount[existingAuthorIndex].likes += blog.likes
+      } else {
+        authorLikeCount.push({
+          author: blog.author,
+          likes: blog.likes
+        })
+      }
+    })
+    const initialvalue = 0
+    const result = authorLikeCount.reduce((previousValue, currentArrayObject, currentIndex, array) => {
+      const maxIndex = array[currentIndex].likes > array[previousValue].likes ? currentIndex : previousValue
+      return maxIndex
+    }, initialvalue)
+    return authorLikeCount[result]
+  }
 }
 
 module.exports = {
   dummy,
   sumOfLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
