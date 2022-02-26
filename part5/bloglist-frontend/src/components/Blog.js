3511478884c3fd6534ce532ee54be6fb72b034d7
hangrybear666 <corey.lauster@gmail.com>
2022-02-26 19:33:21 +0100

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,37 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    <b style={{color: '#222222', fontSize:15, fontFamily: 'consolas'}}>{blog.title}</b>&nbsp;|&nbsp;
-    <i>{blog.author}</i>&nbsp;|&nbsp;
-    <u style={{color: 'purple'}}>{blog.url}</u>&nbsp;|&nbsp;
-    likes: <b style={{color: 'blue'}}>{blog.likes}</b>
-  </div>
-)
+import { useState } from 'react'
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(true)
+
+  const viewInfo = { display: visible ? '' : 'none' }
+  const hideInfo = { display: visible ? 'none' : '' }
+
+  return (
+    <div className="blogContainer">
+      <div className="blogInfo">
+        <b style={{color: '#222222', fontSize:15, fontFamily: 'consolas'}}>{blog.title}</b>&nbsp;
+        <i>{blog.author}</i>&nbsp;
+        <button
+          style={hideInfo}
+          onClick={() => setVisible(true)}>
+          hide
+        </button>
+      </div>
+      <div style={hideInfo}>
+        <u style={{color: 'purple'}}>{blog.url}</u>&nbsp;
+        <p>likes: <b style={{color: 'blue'}}>{blog.likes}</b>&nbsp;
+          <button className="likeBtn">like</button>
+        </p>
+        <p><i>created by {blog.user.name}</i></p>
+      </div>
+      <div style={viewInfo} className="blogInfo">
+        <button
+          onClick={() => setVisible(false)}>
+          view
+        </button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
--- part5/bloglist-frontend/src/components/Logout.js
+++ part5/bloglist-frontend/src/components/Logout.js
@@ -2,7 +2,7 @@ const Logout = ({ user ,handleLogout }) => {
   if (user !== null) {
     return (
       <div style={{ marginTop:15 }}>
-        <i>Logged in as</i> <b>{user.name}</b>
+        <i>Logged in as</i><b>&nbsp;{user.name}</b>&nbsp;
         <button onClick={handleLogout}>
           logout
         </button>
